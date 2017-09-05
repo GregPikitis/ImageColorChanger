0b932d65d86ed7e11eef809bab86f86217c5b51a
Emelia Blankenship <egblip@gmail.com>
2017-09-04 23:42:16 -0400

    The entire thing!
    This project is already kinda done... recently updated to Python 3.0!!

---
+++ imagecolorchanger.py
@@ -0,0 +1,60 @@
+#Hey there!
+#Welcome to the image recolor thing, which is just saying hey I want these pictures all to be this color.
+#I would do this with PNGs that have transparency, i feel like jpgs would just make the whole thing that color.
+#Magick Image needs to install for this program to run, as it is basically just a script for that haha.
+#Anyways, egblip.com 2017 :)
+
+import os
+import time
+import sys
+from tkinter import Tk
+from tkinter import filedialog
+
+Tk().withdraw()
+
+def validateColor(newcolor, hexchar):
+	if len(newcolor) != 6:
+		return False
+
+	for c in newcolor:
+		if c not in hexchar:
+			return False
+
+	return True
+
+def getColor(newcolor, hexchar):
+	while validateColor(newcolor, hexchar) == False:
+		newcolor = input("What color should the images change to? (hexidecimal)\n")
+		newcolor = str(newcolor)
+		newcolor.replace('#','')
+
+		validateColor(newcolor, hexchar)
+
+	newcolor = '#' + newcolor
+
+	return newcolor
+
+def main():
+	newcolor = ""
+	images = ();
+	hexchar = 'ABCDEFabcdef0123456789'
+
+	print("Hello! Welcome to a thing to recolor a bunch of images at once!\n!!WARNING!! This program overwrites the orignal picture. Make sure you are sure.\n")
+
+	time.sleep(2)
+
+	images = filedialog.askopenfilenames(filetypes = (("Image Files", "*.jpeg;*.png;*.jpg;*.gif"), ("All Files", "*.*") ))
+
+	newcolor = getColor(newcolor, hexchar)
+
+	print("\nOkay, here we go!\n")
+
+	for i in images:
+		i = str(i)
+		os.system('magick convert ' + i + ' -fill "' + newcolor + '" -colorize 100% ' + i)
+
+	print("Complete. See ReadMe if you have difficulties")
+	
+	time.sleep(2)
+
+main()
